c5cc6168680bd8341e685efe71a43eb591add054
Jason <jason@jasons-mbp-4.lan>
2019-03-13 13:45:34 -0400

    more minimom

--- docker/simple-mongodb-connection-tester/simple-connection-test.py
+++ docker/simple-mongodb-connection-tester/simple-connection-test.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient
 import sys
 import uuid
+import time
 
-if not len(sys.argv)==2:
-  print "Usage: test-mongo-connection.py <MongoDB Connection String>"
+num_docs = 1000
+
+if not len(sys.argv)>=2:
+  print "Usage: test-mongo-connection.py <MongoDB Connection String> <number_docs>"
   sys.exit("No connection string detected.")
   
+if len(sys.argv)==3:
+  num_docs = int(sys.argv[2])
 
 uri = sys.argv[1]
 print "simple-connection-test: testing connection to %s" % uri
@@ -14,7 +19,7 @@ print "Creating and reading 100 docs in the '%s.foo' namespace" % temp_db_name
 #uri = "mongodb+srv://newyork-service.mongodb.svc.cluster.local/?ssl=false"
 client = MongoClient(uri)
 print client[temp_db_name]
-for i in range(0,100):
+for i in range(0,num_docs):
   client[temp_db_name]['foo'].insert_one( { "i" : i } )
 for doc in client[temp_db_name]['foo'].find():
   print doc
